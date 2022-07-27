2287d94aae4c3a57e719e8eaf534d07b743da7f1
Mark Nicol <marc.nuc@gmail.com>
2022-07-27 12:16:13 -0400

    up

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from "./App";
 
-
+ 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
- 
+  
       <App />
 
     </BrowserRouter>
