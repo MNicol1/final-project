6a354d8f50d236a181765af6d9311d984a2cc920
Mark Nicol <marc.nuc@gmail.com>
2025-03-29 23:22:56 -0400

    app name change

--- client/src/hooks/useRadio.js
+++ client/src/hooks/useRadio.js
@@ -6,7 +6,7 @@ import { RadioBrowserApi } from "radio-browser-api";
 const stationsCache = {}; // Cache object to store stations data
 const cacheDuration = 48 * 60 * 60 * 1000;
 
-const browserRadioApi = new RadioBrowserApi("WorldWaveRadio/1.0");
+const browserRadioApi = new RadioBrowserApi("WorldWaveRadio (formerly My Radio App)");
 browserRadioApi.setBaseUrl("https://fi1.api.radio-browser.info");
 
 const useRadio = ({ country, limit = 8 }) => {
